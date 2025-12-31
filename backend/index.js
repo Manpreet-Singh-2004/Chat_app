110a5887a49e3c6c1fb205577c547433cb920a30
Manpreet.Singh18 <manpreet.singh18@student.ufv.ca>
2025-12-30 22:31:32 -0800

    frontend init

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,29 @@ import {createServer} from 'node:http'
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server)
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"],
+  },
+})
 
 io.on('connection', (socket) => {
-    console.log(`A user is connected: ${socket}`)
+  console.log("A new user is connected: ", socket.id)
+
+  socket.on("message", msg => {
+    io.emit("message", msg)
+  });
+
+  socket.on("disconnect", () =>{
+    console.log("User disconnected: ", socket.id)
+  })
+})
+
+app.get("/api/health", (req, res) =>{
+  res.json({ok: true})
 })
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+  console.log("Backend running on http://localhost:3000")
+})
